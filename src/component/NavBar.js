1d97c45dd44eb7d4c1e29aafdbe93de007ccbef9
Its-JoeTheKing <errfig22@gmail.com>
2022-10-01 22:44:39 +0100

    first commit

---
+++ src/component/NavBar.js
@@ -0,0 +1,15 @@
+import 'react';
+import React, { Component } from 'react'
+import {Navbar,NavbarBrand,NavbarText,NavLink} from 'reactstrap'
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar className='nav'>
+
+        </Navbar>
+      </div>
+    )
+  }
+}
