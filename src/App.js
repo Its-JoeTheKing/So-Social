10595a17a7101641754af2700ea50a4bfea734b6
Its-JoeTheKing <errfig22@gmail.com>
2022-10-02 11:50:44 +0100

    piped

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {useEffect,useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import NavBar from '../src/component/NavBar';
+import NavBars from '../src/component/NavBar';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true'/>
       <link href="https://fonts.googleapis.com/css2?family=Acme&family=Bangers&family=Bree+Serif&family=Josefin+Sans&family=Montserrat:wght@500&family=Poppins:wght@500&family=Roboto:wght@500&family=Secular+One&family=Signika+Negative&family=Staatliches&family=Teko&display=swap" rel="stylesheet"/>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-      <NavBar/>
+      <NavBars/>
     </div>
   );
 }
